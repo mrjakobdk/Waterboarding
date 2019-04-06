8e5eeff43a9f6776018886f4a55f51aaeff52cea
mrjakobdk <mrjakobdk@gmail.com>
2019-04-06 10:43:21 +0200

    First!

--- main.py
+++ main.py
@@ -1,36 +1,14 @@
-#!/usr/bin/env python
-from flask import Flask, render_template, Response
+import datetime
 import picamera
-import cv2
-import socket
-import io
-
-app = Flask(__name__)
-vc = cv2.VideoCapture(0)
+import time
 
 
-@app.route('/')
-def index():
-    """Video streaming"""
-    return render_template('index.html')
+date = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
 
-def gen():
-    """Video streaming generator function."""
-    while True:
-        rval, frame = vc.read()
-        cv2.imwrite('t.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
-
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    camera.start_recording("/home/pi/" + date + "video.h264")
+    camera.wait_recording(30)
+    camera.stop_recording()
+    camera.stop_preview()
